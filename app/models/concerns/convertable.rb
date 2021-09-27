ee9151f52c0d567dd37662f4127a8b69fe0d9907
Taylor Varoglu <tava8398@colorado.edu>
2021-09-26 18:57:56 -0600

    Update styling per Rubocop

--- app/models/concerns/convertable.rb
+++ app/models/concerns/convertable.rb
@@ -1,9 +1,9 @@
 module Convertable
   def integer_to_datetime(int)
-    Time.at(int)
+    Time.at(int).getlocal
   end
 
   def kelvin_to_fahrenheit(temp)
-    ((temp - 273.15) * 9/5.to_f + 32).round(2)
+    (((temp - 273.15) * 9 / 5.to_f) + 32).round(2)
   end
 end
