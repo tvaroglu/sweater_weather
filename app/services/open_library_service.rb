4833bca065f2b16938b1bec2df7b455e88dfc2b6
Taylor Varoglu <tava8398@colorado.edu>
2021-09-27 09:16:54 -0600

    Add OpenLibraryService to /services

---
+++ app/services/open_library_service.rb
@@ -0,0 +1,16 @@
+class OpenLibraryService < ThirdPartyService
+  class << self
+    def base_url
+      'https://openlibrary.org'
+    end
+
+    def get_books(search_params)
+      response = conn(base_url).get("/search.json?title=#{reformat(search_params)}")
+      render_request(response.body)
+    end
+
+    def reformat(search_params)
+      search_params.gsub(/,/, '+')
+    end
+  end
+end
